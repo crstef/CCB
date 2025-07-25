65f0b9533360e8ea5d9c54741917087e0d136555
Cristian Stef <84433169+crstef@users.noreply.github.com>
2025-07-25 16:37:05 +0000

    sec

--- wave/resources/views/widgets/welcome-widget.blade.php
+++ wave/resources/views/widgets/welcome-widget.blade.php
@@ -5,33 +5,26 @@
 <x-filament-widgets::widget class="fi-account-widget">
     <x-filament::section>
         <div class="flex gap-x-3 items-center">
-
             <div class="flex-1">
-                <h2
-                    class="grid flex-1 text-base font-semibold leading-6 text-gray-950 dark:text-white"
-                >
+                <h2 class="grid flex-1 text-base font-semibold leading-6 text-gray-950 dark:text-white">
                     Bine ai venit in CCB Admin
                 </h2>
-
                 <p class="text-sm text-gray-500 dark:text-gray-400">
                     Folosești tema Tallstack
                 </p>
             </div>
-
-            
-
-                <x-filament::button
-                    color="gray"
-                    icon="heroicon-m-arrow-top-right-on-square"
-                    icon-alias="panels::widgets.account.logout-button"
-                    labeled-from="sm"
-                    tag="a"
-                    type="submit"
-                    href="/"
-                    target="_blank"
-                >
-                    Viziteaza Site-ul
-                </x-filament::button>
+            <x-filament::button
+                color="gray"
+                icon="heroicon-m-arrow-top-right-on-square"
+                icon-alias="panels::widgets.account.logout-button"
+                labeled-from="sm"
+                tag="a"
+                type="submit"
+                href="/"
+                target="_blank"
+            >
+                Viziteaza Site-ul
+            </x-filament::button>
         </div>
     </x-filament::section>
 </x-filament-widgets::widget>
