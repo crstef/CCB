75c679961cb7144d68c5f4ace3cf8e75f9e3d7d0
Cristian Stef <84433169+crstef@users.noreply.github.com>
2025-07-25 16:18:54 +0000

    second try

--- wave/resources/views/widgets/welcome-widget.blade.php
+++ wave/resources/views/widgets/welcome-widget.blade.php
@@ -10,7 +10,7 @@
                 <h2
                     class="grid flex-1 text-base font-semibold leading-6 text-gray-950 dark:text-white"
                 >
-                    Bine ai venit în Wave Admin
+                    Bine ai venit în CCB Admin
                 </h2>
 
                 <p class="text-sm text-gray-500 dark:text-gray-400">
