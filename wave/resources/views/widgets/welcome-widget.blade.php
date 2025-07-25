fdf9672f9ef752f7e0707d1c272e00c887542029
Cristian Stef <84433169+crstef@users.noreply.github.com>
2025-07-25 16:29:15 +0000

    manual done

--- wave/resources/views/widgets/welcome-widget.blade.php
+++ wave/resources/views/widgets/welcome-widget.blade.php
@@ -5,33 +5,33 @@
 <x-filament-widgets::widget class="fi-account-widget">
     <x-filament::section>
         <div class="flex gap-x-3 items-center">
+
             <div class="flex-1">
-                <h2 class="grid flex-1 text-base font-semibold leading-6 text-gray-950 dark:text-white">
-                    Bine ai venit în CCB Admin
+                <h2
+                    class="grid flex-1 text-base font-semibold leading-6 text-gray-950 dark:text-white"
+                >
+                    Bine ai venit in CCB Admin
                 </h2>
+
                 <p class="text-sm text-gray-500 dark:text-gray-400">
                     Folosești tema Tallstack
                 </p>
             </div>
-            <x-filament::button
-                color="gray"
-                icon="heroicon-m-arrow-top-right-on-square"
-                icon-alias="panels::widgets.account.logout-button"
-                labeled-from="sm"
-                tag="a"
-                type="submit"
-                href="/"
-                target="_blank"
-            >
-                Vizitează site-ul
-            </x-filament::button>
+
+            
+
+                <x-filament::button
+                    color="gray"
+                    icon="heroicon-m-arrow-top-right-on-square"
+                    icon-alias="panels::widgets.account.logout-button"
+                    labeled-from="sm"
+                    tag="a"
+                    type="submit"
+                    href="/"
+                    target="_blank"
+                >
+                    Viziteaza Site-ul
+                </x-filament::button>
         </div>
-    </x-filament::section>
-</x-filament-widgets::widget>
-                Vizitează site-ul
-            </x-filament::button>
-        </div>
-    </x-filament::section>
-</x-filament-widgets::widget>
     </x-filament::section>
 </x-filament-widgets::widget>
