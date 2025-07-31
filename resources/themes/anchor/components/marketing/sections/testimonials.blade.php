64e55bac032f4730a9a662ca576d09f518cfa034
Cristian Stef <84433169+crstef@users.noreply.github.com>
2025-07-31 07:06:54 +0000

    echipa

--- resources/themes/anchor/components/marketing/sections/testimonials.blade.php
+++ resources/themes/anchor/components/marketing/sections/testimonials.blade.php
@@ -3,17 +3,17 @@
     <ul role="list" class="grid grid-cols-1 gap-12 py-12 mx-auto max-w-2xl lg:max-w-none lg:grid-cols-3">
         <li>
             <figure class="flex flex-col justify-between h-full">
-                <blockquote class="">
+               {{-- <blockquote class="">
                     <p class="text-sm sm:text-base font-medium text-zinc-500">
                         O descriere scurta a fiecarui membru din echipa
                     </p>
-                </blockquote>
+                </blockquote> --}}
                 <figcaption class="flex flex-col justify-between mt-6">
                     <img alt="#_" src="https://cdn.devdojo.com/images/june2024/adam.jpeg" class="object-cover rounded-full grayscale size-14">
                     <div class="mt-4">
-                        <div class="font-medium text-zinc-900">Membru1</div>
+                        <div class="font-medium text-zinc-900">Panoiu Gabirel</div>
                         <div class="mt-1 text-sm text-zinc-500">
-                            Director CCB
+                            Presedinte CCB
                         </div>
                     </div>
                 </figcaption>
@@ -29,9 +29,9 @@
                 <figcaption class="flex flex-col justify-between mt-6">
                     <img alt="#_" src="https://cdn.devdojo.com/images/june2024/caleb.jpeg" class="object-cover rounded-full grayscale size-14">
                     <div class="mt-4">
-                        <div class="font-medium text-zinc-900">Membru 2</div>
+                        <div class="font-medium text-zinc-900">Cosmin Pop Moldovan</div>
                         <div class="mt-1 text-sm text-zinc-500">
-                            functie membru 2
+                            Vicepresedinte CCB- responsabil mondioring
                         </div>
                     </div>
                 </figcaption>
@@ -47,9 +47,45 @@
                 <figcaption class="flex flex-col justify-between mt-6">
                     <img alt="#_" src="https://cdn.devdojo.com/images/june2024/taylor.jpeg" class="object-cover rounded-full grayscale size-14">
                     <div class="mt-4">
-                        <div class="font-medium text-zinc-900">Membru 3</div>
+                        <div class="font-medium text-zinc-900">Attila Toth</div>
                         <div class="mt-1 text-sm text-zinc-500">
-                            Fondator eg
+                            Vicepresedinte CCB- responsabil IGP
+                        </div>
+                    </div>
+                </figcaption>
+            </figure>
+        </li>
+        <li>
+            <figure class="flex flex-col justify-between h-full">
+                <blockquote class="">
+                    <p class="text-sm sm:text-base font-medium text-zinc-500">
+                        Descriere membru 3
+                    </p>
+                </blockquote>
+                <figcaption class="flex flex-col justify-between mt-6">
+                    <img alt="#_" src="https://cdn.devdojo.com/images/june2024/taylor.jpeg" class="object-cover rounded-full grayscale size-14">
+                    <div class="mt-4">
+                        <div class="font-medium text-zinc-900">Dan Zloteanu</div>
+                        <div class="mt-1 text-sm text-zinc-500">
+                            Vicepresedinte CCB- responsabil Agility
+                        </div>
+                    </div>
+                </figcaption>
+            </figure>
+        </li>
+        <li>
+            <figure class="flex flex-col justify-between h-full">
+                <blockquote class="">
+                    <p class="text-sm sm:text-base font-medium text-zinc-500">
+                        Descriere membru 3
+                    </p>
+                </blockquote>
+                <figcaption class="flex flex-col justify-between mt-6">
+                    <img alt="#_" src="https://cdn.devdojo.com/images/june2024/taylor.jpeg" class="object-cover rounded-full grayscale size-14">
+                    <div class="mt-4">
+                        <div class="font-medium text-zinc-900">Flaviu Bajan</div>
+                        <div class="mt-1 text-sm text-zinc-500">
+                            Vicepresedinte CCB- responsabil Organizare generala
                         </div>
                     </div>
                 </figcaption>
