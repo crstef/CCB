6f1ddf411982961a9615bc64855b33cf97aefb7a
Cristian Stef <84433169+crstef@users.noreply.github.com>
2025-08-26 07:26:05 +0000

    all documents

--- resources/views/livewire/documents-carousel-fresh.blade.php
+++ resources/views/livewire/documents-carousel-fresh.blade.php
@@ -3,12 +3,13 @@
         <h2>Documents Carousel Fresh Test</h2>
         <p>Count: {{ $documents->count() }}</p>
         
-        @if($documents->count() > 0)
-            @php $doc = $documents->first(); @endphp
-            <div class="bg-white p-3 rounded mt-2">
-                <h3>{{ $doc->title }}</h3>
-                <small>{{ $doc->created_at->format('d.m.Y') }}</small>
-            </div>
-        @endif
+        <div class="flex gap-2 mt-2">
+            @foreach($documents as $document)
+                <div class="bg-white p-3 rounded">
+                    <h3>{{ $document->title }}</h3>
+                    <small>{{ $document->created_at->format('d.m.Y') }}</small>
+                </div>
+            @endforeach
+        </div>
     </div>
 </div>
