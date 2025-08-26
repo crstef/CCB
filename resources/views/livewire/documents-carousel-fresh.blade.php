a78a9c20b05a9ec22017922b424cfc13e5c0cc15
Cristian Stef <84433169+crstef@users.noreply.github.com>
2025-08-26 07:23:47 +0000

    s

--- resources/views/livewire/documents-carousel-fresh.blade.php
+++ resources/views/livewire/documents-carousel-fresh.blade.php
@@ -2,5 +2,13 @@
     <div class="p-4 bg-gray-100 rounded">
         <h2>Documents Carousel Fresh Test</h2>
         <p>Count: {{ $documents->count() }}</p>
+        
+        @if($documents->count() > 0)
+            @php $doc = $documents->first(); @endphp
+            <div class="bg-white p-3 rounded mt-2">
+                <h3>{{ $doc->title }}</h3>
+                <small>{{ $doc->created_at->format('d.m.Y') }}</small>
+            </div>
+        @endif
     </div>
 </div>
