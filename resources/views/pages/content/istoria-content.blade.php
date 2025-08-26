23de8c137a1c52ffa93baa54a59fb758bbbaa996
Cristian Stef <84433169+crstef@users.noreply.github.com>
2025-08-26 11:37:00 +0000

    first page tunning

---
+++ resources/views/pages/content/istoria-content.blade.php
@@ -0,0 +1,7 @@
+<div class="max-w-4xl mx-auto py-12 px-4">
+    <div class="bg-gradient-to-r from-amber-600 to-orange-700 rounded-2xl p-8 mb-8 text-white">
+        <p class="text-xl opacity-90">Descoperă fascinanta poveste a dezvoltării rasei Ciobanesc Belgian de-a lungul secolelor</p>
+    </div>
+    
+    <!-- Restul conținutului... -->
+</div>
