ae78ee74750d1b4d5eb155024deff4d6b5ac63ac
Cristian Stef <84433169+crstef@users.noreply.github.com>
2025-08-26 11:59:44 +0000

    icons

--- resources/themes/anchor/components/marketing/sections/testimonials.blade.php
+++ resources/themes/anchor/components/marketing/sections/testimonials.blade.php
@@ -9,7 +9,9 @@
                     </p>
                 </blockquote> --}}
                 <figcaption class="flex flex-col justify-between mt-6">
-                    <img alt="#_" src="https://cdn.devdojo.com/images/june2024/adam.jpeg" class="object-cover rounded-full grayscale size-14">
+                    <svg class="w-10 h-10 text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z"></path>
+                    </svg>
                     <div class="mt-4">
                         <div class="font-medium text-zinc-900">Panoiu Gabirel</div>
                         <div class="mt-1 text-sm text-zinc-500">
@@ -22,7 +24,9 @@
         <li>
             <figure class="flex flex-col justify-between h-full">
                 <figcaption class="flex flex-col justify-between mt-6">
-                    <img alt="#_" src="https://cdn.devdojo.com/images/june2024/caleb.jpeg" class="object-cover rounded-full grayscale size-14">
+                    <svg class="w-10 h-10 text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z"></path>
+                    </svg>
                     <div class="mt-4">
                         <div class="font-medium text-zinc-900">Cosmin Pop Moldovan</div>
                         <div class="mt-1 text-sm text-zinc-500">
@@ -35,7 +39,9 @@
         <li>
             <figure class="flex flex-col justify-between h-full">
                 <figcaption class="flex flex-col justify-between mt-6">
-                    <img alt="#_" src="https://cdn.devdojo.com/images/june2024/taylor.jpeg" class="object-cover rounded-full grayscale size-14">
+                    <svg class="w-10 h-10 text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"></path>
+                    </svg>
                     <div class="mt-4">
                         <div class="font-medium text-zinc-900">Attila Toth</div>
                         <div class="mt-1 text-sm text-zinc-500">
@@ -48,7 +54,9 @@
         <li>
             <figure class="flex flex-col justify-between h-full">
                 <figcaption class="flex flex-col justify-between mt-6">
-                    <img alt="#_" src="https://cdn.devdojo.com/images/june2024/taylor.jpeg" class="object-cover rounded-full grayscale size-14">
+                    <svg class="w-10 h-10 text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path>
+                    </svg>
                     <div class="mt-4">
                         <div class="font-medium text-zinc-900">Dan Zloteanu</div>
                         <div class="mt-1 text-sm text-zinc-500">
