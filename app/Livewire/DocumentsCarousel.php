13c43e9b7f4895f92786c11383b431761391ebf8
Cristian Stef <stef.cristian3@gmail.com>
2025-09-01 07:25:36 +0200

    Mesajul commitului

---
+++ app/Livewire/DocumentsCarousel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class DocumentsCarousel extends Component
+{
+    public function render()
+    {
+        return view('livewire.documents-carousel');
+    }
+}
