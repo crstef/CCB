d615eb16492ec3999b340b067071a87ca034dbec
Cristian Stef <84433169+crstef@users.noreply.github.com>
2025-09-08 08:29:19 +0000

    Update posts-pages-widget to display event count instead of post count

--- wave/resources/views/widgets/posts-pages-widget.blade.php
+++ wave/resources/views/widgets/posts-pages-widget.blade.php
@@ -2,17 +2,17 @@
     <x-filament::section class="w-full">
         <div class="flex gap-x-5 items-center">
             <div class="flex-">
-                <x-phosphor-pencil-line-duotone class="h-10 text-blue-600 fill-current" />
+                <x-phosphor-calendar-blank-duotone class="h-10 text-blue-600 fill-current" />
             </div>
             
             <div class="w-full">
                 <div class="flex flex-col">
-                    <div class="mt-1 text-2xl font-semibold tracking-tight text-gray-900 dark:text-gray-200">{{ \Wave\Post::count() }}</div>
+                    <div class="mt-1 text-2xl font-semibold tracking-tight text-gray-900 dark:text-gray-200">{{ \Wave\Event::count() }}</div>
                 </div>
             </div>
             
         </div>
-        <div class="mt-2 text-xs font-medium text-gray-500 truncate">Total articole postate</div>
+        <div class="mt-2 text-xs font-medium text-gray-500 truncate">Total evenimente</div>
     </x-filament::section>
     <x-filament::section class="w-full">
         <div class="flex gap-x-5 items-center">
