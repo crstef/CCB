7b9661f409779a2d46db2b8312bfecb56e639a7b
Cristian Stef <84433169+crstef@users.noreply.github.com>
2025-09-11 14:04:06 +0000

    Update contact link in footer to use named route for improved navigation

--- resources/themes/anchor/partials/footer.blade.php
+++ resources/themes/anchor/partials/footer.blade.php
@@ -127,7 +127,7 @@
                             </a>
                         </li>
                         <li>
-                            <a href="#_" class="relative inline-block text-black group">
+                            <a href="{{ route('contact') }}" class="relative inline-block text-black group">
                                 <span class="absolute bottom-0 w-full transition duration-150 ease-out transform -translate-y-1 border-b border-black opacity-0 group-hover:opacity-100 group-hover:translate-y-1"></span>
                                 <span>Contact</span>
                             </a>
