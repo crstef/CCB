74bcb96a4d2b22d0cd8888cb08799ce6a8005a35
Cristian Stef <84433169+crstef@users.noreply.github.com>
2025-10-01 05:45:35 +0000

    fix: Adjust position of president's stamp for improved layout on member card

--- resources/themes/anchor/pages/dashboard/index.blade.php
+++ resources/themes/anchor/pages/dashboard/index.blade.php
@@ -111,11 +111,11 @@
                                     <span class="font-semibold text-sm">Perioada de valabilitate</span>
                                     <span class="ml-4 font-bold">{{ date('Y') - 1 }} - {{ date('Y') }}</span>
                                 </div>
-                            </div>
-                            <div class="text-xs text-right relative">
                                 <div class="relative top-12 -right-8 w-28 h-20 z-20">
                                     <img src="/images/stampila_presedinte.png" alt="Ștampila Președinte CCB" class="w-full h-full object-contain opacity-80">
                                 </div>
+                            </div>
+                            <div class="text-xs text-right relative">
                                 {{-- Ștampila și semnătura președintelui --}}                             
                                 <div class="font-semibold">Președinte CCB</div>
                                 <div class="mt-1">Gabriel Panoiu</div>
